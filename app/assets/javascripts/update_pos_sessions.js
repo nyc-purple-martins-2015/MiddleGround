5d03a20410553f3219b6bdd3a267a720047971fd
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-19 18:10:35 -0500

    Update Gemfile for heroku

--- app/assets/javascripts/update_pos_sessions.js
+++ app/assets/javascripts/update_pos_sessions.js
@@ -17,5 +17,9 @@
         $('#location_session').on('click',function(){
           var pos = getLocation();
           showPosition(pos);
+<<<<<<< HEAD
       });
+=======
+      })
+>>>>>>> Update Gemfile for heroku
       });
