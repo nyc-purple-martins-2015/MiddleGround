457387f2df8cc8c9872c2f642630cb97903adfaf
Amy Liao <amyhliao88@gmail.com>
2015-11-20 08:44:46 -0800

    add functions to callback and failure method in auth0 controller

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -1,7 +1,16 @@
 class Auth0Controller < ApplicationController
   def callback
+    user = User.find_or_create_by(uid: request.env['omniauth.auth']['uid'])
+    session[:userinfo] = request.env['omniauth.auth']
+    user.username = request.env['omniauth.auth']['info']['name']
+    user.save
+    session[:user_id] = user.id
+    session[:username] = user.username
+    redirect_to root_path
   end
 
   def failure
+    # show a failure page or redirect to an error page
+    @error_msg = request.params['message']
   end
 end
