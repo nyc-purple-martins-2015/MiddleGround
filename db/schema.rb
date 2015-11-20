2b35bda22324d87f2efc2a9fcb4cba471b608a7b
Anders Drew <anders.j.drew@gmail.com>
2015-11-20 15:16:32 -0500

    adds pending to friendship table, adds friends and activities methods to user model

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20151119230833) do
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest", null: false
-    t.integer  "phone_number",    null: false
+    t.string   "phone_number",    null: false
     t.string   "email",           null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
