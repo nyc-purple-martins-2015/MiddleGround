495a3b13e50d5012b40dca74e64791a7c3bdc7ab
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-19 21:00:53 -0500

    Add user model test; tests pass

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    username "username"
+    password "password"
+    email "thing@example.com"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
-describe User do
-  it { is_expected.to validate_presence_of(:username) }
-  it { is_expected.to validate_uniqueness_of(:email) }
-  it { is_expected.to validate_length_of(:password).is_at_least(10) }
+require 'rails_helper'
+
+RSpec.describe User do
+
+  it { is_expected.to validate_presence_of :username }
+  it { is_expected.to validate_presence_of :password }
+  it { is_expected.to validate_presence_of :email }
+
 end
