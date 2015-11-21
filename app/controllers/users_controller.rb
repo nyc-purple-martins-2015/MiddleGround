9619e30ed062fa6ca195769a0e9434d682d291b8
Alicia <email": null>
2015-11-20 20:29:46 -0500

    Add activities routes and lat/long to table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,3 @@
 class UsersController < ApplicationController
 
-  def index
-  end
-
-  def geocoding
-    respond_to do |format|
-    format.js
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119230833) do
+ActiveRecord::Schema.define(version: 20151121012143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20151119230833) do
     t.integer  "friend_id",  null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "lat"
+    t.float    "long"
   end
 
   create_table "addresses", force: :cascade do |t|
