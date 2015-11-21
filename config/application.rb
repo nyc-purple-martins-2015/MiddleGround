439bc49109cbc0f85b2f8eceacf86d92a56254aa
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-21 18:14:12 -0500

    Add statement to config/application

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module Middleground
 
     # config.assets.initialize_on_precompile = false
 
+    # config.assets.initialize_on_precompile = false
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
