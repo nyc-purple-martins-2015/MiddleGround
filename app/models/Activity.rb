a20754ea88ace1df98bff0e97083e5cb1cdb906c
Alicia <email": null>
2015-11-20 21:32:04 -0500

    Add geokit gem

--- app/models/Activity.rb
+++ app/models/Activity.rb
@@ -4,15 +4,10 @@ class Activity < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
   belongs_to :friend, class_name: 'User'
 
-<<<<<<< 36d9df095939915538a13d8846ebbd19c17d2736
-
   acts_as_mappable :default_formula => :sphere,
                    :distance_field_name => :distance,
                    :lat_column_name => :lat,
                    :lng_column_name => :long
-   attr_accessor :address, :lat, :long
 
-=======
-   attr_accessor :address, :lat, :long
->>>>>>> Take out trial js code
+  attr_accessor :address, :lat, :long
 end
