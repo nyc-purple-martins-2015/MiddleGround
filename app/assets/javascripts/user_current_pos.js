27b581251fd75f26cb242a64b7b8b7ff04af802d
Alicia <email": null>
2015-11-20 20:29:46 -0500

    Add activities routes and lat/long to table

--- app/assets/javascripts/user_current_pos.js
+++ app/assets/javascripts/user_current_pos.js
@@ -1,22 +1,36 @@
-$(document).ready(function(){
 
-  $(window).load(function(){
-  function initialize() {
-    var mapCanvas = document.getElementById('map');
-    var map = new google.maps.Map(mapCanvas);
-  }
-  function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+function initMap() {
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success);
+} else {
+  error('Geo Location is not supported');
+}
 }
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+navigator.geolocation.getCurrentPosition(success);
+
+function success(position) {
+     var lat = position.coords.latitude;
+     var long = -position.coords.longitude;
+debugger;
 }
 
+var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var options = {
+    zoom: 15,
+    center: coords,
+    mapTypeControl: false,
+    navigationControlOptions: {
+      style: google.maps.NavigationControlStyle.SMALL
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_container"), options);
+
+  var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title:"You are here!"
   });
-});
+
+
