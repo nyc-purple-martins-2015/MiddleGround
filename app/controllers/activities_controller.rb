91e5f393bda2a9fb088aac9345e4d61b76b31574
Alicia <email": null>
2015-11-20 21:02:32 -0500

    Take out trial js code

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,10 +7,6 @@ class ActivitiesController < ApplicationController
     }
     # render json: Yelp.client.search('Waterford, CT', params).businesses.map{|business| "#{business.name} #{business.location.display_address}"}.sample
     render json: Yelp.client.search("cll= 40.7, 74.0", params).businesses.map {|business| business.name }
-<<<<<<< 8a8ef2f582a65212225e9addf32e684f3c391d80
-=======
-
->>>>>>> Add activities routes and lat/long to table
   end
 
   def new
@@ -20,21 +16,13 @@ class ActivitiesController < ApplicationController
   end
 
   def show
-<<<<<<< 8a8ef2f582a65212225e9addf32e684f3c391d80
+
     @activity = Activity.find_by(activity_params)
-=======
-    @activity = Activity.find(1)
->>>>>>> Add activities routes and lat/long to table
+
   end
 
-  def index
-    @activities = Activity.all
-<<<<<<< 8a8ef2f582a65212225e9addf32e684f3c391d80
-  end
 
   def activity_params
       params.require(:activity).permit(:address, :lat,:long)
-=======
->>>>>>> Add activities routes and lat/long to table
   end
 end
