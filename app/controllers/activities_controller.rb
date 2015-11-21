3bbc689e8f69b77e4505e1b92762aab2ae0fb957
Alicia <email": null>
2015-11-20 21:02:32 -0500

    Take out trial js code

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,6 +7,10 @@ class ActivitiesController < ApplicationController
     }
     # render json: Yelp.client.search('Waterford, CT', params).businesses.map{|business| "#{business.name} #{business.location.display_address}"}.sample
     render json: Yelp.client.search("cll= 40.7, 74.0", params).businesses.map {|business| business.name }
+<<<<<<< b0e03e78a48946bd7c01c3a240d913367f4ee134
+=======
+
+>>>>>>> Take out trial js code
   end
 
   def new
@@ -16,8 +20,21 @@ class ActivitiesController < ApplicationController
   end
 
   def show
+<<<<<<< b0e03e78a48946bd7c01c3a240d913367f4ee134
     @activity = Activity.find_by(activity_params)
   end
+
+
+=======
+
+    @activity = Activity.find_by(activity_params)
+
+  end
+
+  def index
+    @activities = Activity.all
+  end
+>>>>>>> Take out trial js code
 
 
 
