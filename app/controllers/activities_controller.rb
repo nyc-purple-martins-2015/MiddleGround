a9eeb7d4413ec8935b0a0c857ec929ce63b1d3be
Amy Liao <amyhliao88@gmail.com>
2015-11-21 10:28:26 -0800

    add logic to create method in activity controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,7 +7,6 @@ class ActivitiesController < ApplicationController
     }
     # render json: Yelp.client.search('Waterford, CT', params).businesses.map{|business| "#{business.name} #{business.location.display_address}"}.sample
     render json: Yelp.client.search("cll= 40.7, 74.0", params).businesses.map {|business| business.name }
-
   end
 
   def new
@@ -15,13 +14,23 @@ class ActivitiesController < ApplicationController
   end
 
   def create
+    activity = Activity.new(activity_params)
+    if activity.save
+      render json: Yelp.client.search(location, params).businesses.map{|business| "#{business.name} #{businessesusiness.location.display_address}"}.sample
+      redirect_to activity_path
+    else
+      flash[:errors] = "Something went wrong with your request. Please try again."
+      redirect_to new_activity_path
+    end
   end
 
   def show
     @activity = Activity.find_by(activity_params)
   end
 
+  private
+
   def activity_params
-      params.require(:activity).permit(:address, :lat,:long)
+    params.require(:activity).permit(:title, :address, :creator_id, :friend_id)
   end
 end
