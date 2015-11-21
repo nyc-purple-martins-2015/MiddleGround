bdef93f2a46dc44ce4114884bf2df47fbd78b08e
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-21 18:16:32 -0500

    Previous commit not needed

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,12 @@ module Middleground
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     config.assets.initialize_on_precompile = false
 >>>>>>> Add statement to config/application
+=======
+>>>>>>> Previous commit not needed
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
