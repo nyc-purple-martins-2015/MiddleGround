57f27606b06e25b76d5b55503289449d2231ba46
Amy Liao <amyhliao88@gmail.com>
2015-11-23 11:39:59 -0800

    add modes of transportation to direction js file

--- app/assets/javascripts/direction.js
+++ app/assets/javascripts/direction.js
@@ -3,22 +3,14 @@ var DirectionMap =  function(position,domMap){
   this.map = new google.maps.Map(document.getElementById('map-image'),{center: position, zoom: 14});
 };
 
-DirectionMap.prototype.createMarker = function(inputPos, label){
-  return new google.maps.Marker({position: inputPos, map: this.map, title: label})
-};
-DirectionMap.prototype.getDirections = function(){
-  var destination = $(".destination").attr("data-destination");
-  var originLat = $(".destination").attr("data-origin-lat");
-  var originLong = $(".destination").attr("data-origin-long");
-}
-
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var selectedMode = document.getElementById('mode').value;
   var end = new google.maps.LatLng({lat: 40.7127, lng: -74.005});
   var start = new google.maps.LatLng({lat: 40.7117, lng: -74.005});
   directionsService.route({
     origin: start,
     destination: end,
-    travelMode: google.maps.TravelMode.WALKING
+    travelMode: google.maps.TravelMode[selectedMode]
   }, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
@@ -32,18 +24,16 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 //   control.style.display = 'block';
 //   map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-document.addEventListener('DOMContentLoaded',function(){
-  var domMap = $('map-image');
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var directionsService = new google.maps.DirectionsService;
-  var map = new DirectionMap(new google.maps.LatLng({lat: 40.7117, lng: -74.005}), domMap);
-  directionsDisplay.setMap(map.map);
-  // map.createMarker(inputPosition, 'Direction Map');
-  // map.createMarker(currentPosition, 'Direction Map');
-  calculateAndDisplayRoute(directionsService, directionsDisplay);
+document.addEventListener('DOMContentLoaded', function(){
+  $("#mode").change(function(event){
 
-
+    var domMap = $('map-image');
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
+    var map = new DirectionMap(new google.maps.LatLng({lat: 40.7117, lng: -74.005}), domMap);
+    directionsDisplay.setMap(map.map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  })
 })
-
 
 
