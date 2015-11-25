55d031936112e0494c6f555a3574656a9b58cb1d
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-25 15:11:15 -0500

    Add flash to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,17 +2,17 @@ class FriendshipsController < ApplicationController
   def create
     friendship = current_user.requested_friendships.new(friendship_params)
     if friendship.save
-      flash.now[:notice] = "Your friend request has been submitted."
+      flash[:notice] = "Your friend request has been submitted."
       redirect_to user_path(current_user)
     else
-      flash.now[:notice] = "Your friend request has not been submitted. Please try again."
+      flash[:notice] = "Your friend request has not been submitted. Please try again."
       redirect_to user_path(current_user)
     end
   end
 
   def accept
     friendship = Friendship.find(params[:id])
-    flash.now[:notice] = "You are now friends with #{ friendship.requester.username }."
+    flash[:notice] = "You are now friends with #{ friendship.requester.username }."
     friendship.update_attributes(pending: 0)
     redirect_to user_path(current_user)
   end
