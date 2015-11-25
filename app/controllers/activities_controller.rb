4d3a10c90a43e4e097694633a5609fd005e73f11
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-25 14:31:27 -0500

    Fix merge conflict

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -14,7 +14,6 @@ class ActivitiesController < ApplicationController
     coords = destination.location.coordinate
     address = destination.location.display_address.join(", ")
     @activity = Activity.new(location: address, title: destination.name, creator_id: current_user.id, friend_id: params[:friend].to_i, lat: coords.latitude, long: coords.longitude)
-    p @activity
     if @activity.save!
       @friend = User.find(@activity.friend_id)
       render :show, layout:false
