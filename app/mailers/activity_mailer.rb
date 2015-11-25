ae8aff30bc820fa46140fc4ef48441d0668d98e7
Alicia MacLennan <aflyingfemme@gmail.com>
2015-11-25 09:20:39 -0500

    Fix email to alert friend only

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -5,7 +5,6 @@ class ActivityMailer < ApplicationMailer
     @user = activity.creator
     @friend = activity.friend
     @url  = 'http://middle-ground.herokuapp.com'
-    mail(to: @user.email, subject: 'You Have a New MiddleGround!')
      mail(to: @friend.email, subject: 'You Have a New MiddleGround!')
   end
 end
